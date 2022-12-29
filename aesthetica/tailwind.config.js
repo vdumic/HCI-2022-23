b3b772633a2fcb2dd3052b3f0da110e3e01f3da4
vdumic <vdumic00@fesb.hr>
2022-12-29 12:18:29 +0100

    Tailwind init

--- aesthetica/tailwind.config.js
+++ aesthetica/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['"DM Serif Display"', "sans-serif"],
+      },
+      colors: {
+        bckgrnd: {
+          light: "#efefefd9",
+          DEFAULT: "#e7e4e4d9",
+          dark: "#bfbbbbd9",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
