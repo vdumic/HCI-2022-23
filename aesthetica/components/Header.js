14d9a874a5e523f289b5dd065a6b34ef7f921179
vdumic <vdumic00@fesb.hr>
2023-01-03 15:50:38 +0100

    Header added

--- aesthetica/components/Header.js
+++ aesthetica/components/Header.js
@@ -1,7 +1,54 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { AiOutlineUser, AiOutlineSearch } from "react-icons/ai";
+import { BiShoppingBag } from "react-icons/bi";
+
 import NavBar from "./NavBar";
 
 const Header = () => {
-  return <NavBar />;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  return (
+    <header
+      className={`sticky top-0 flex items-center bg-bckgrnd-light ${
+        isScrolled && "bg-bckgrnd"
+      }`}
+    >
+      <main className="max-w-full mx-auto flex-grow flex flex-col my-5">
+        <div className="flex items-center justify-between mx-24">
+          <Link href="/">
+            <h1 className="font-bold text-3xl">Aesthetica</h1>
+          </Link>
+          <NavBar />
+          <div className="flex items-center space-x-4 text-sm font-light">
+            <AiOutlineSearch className="sm hidden h-6 w-6 sm:inline cursor-pointer" />
+            <Link href="/profile">
+              <AiOutlineUser className="sm hidden h-6 w-6 sm:inline cursor-pointer" />
+            </Link>
+            <Link href="/shoppingBag">
+              <BiShoppingBag className="sm hidden h-6 w-6 sm:inline cursor-pointer" />
+            </Link>
+          </div>
+        </div>
+      </main>
+    </header>
+  );
 };
 
 export default Header;
---
+++ aesthetica/pages/about.js
@@ -0,0 +1,11 @@
+import HeaderFooterLayout from "../layouts/HeaderFooterLayout";
+
+const About = () => {
+  return (
+    <HeaderFooterLayout>
+      <div>ABOUT</div>
+    </HeaderFooterLayout>
+  );
+};
+
+export default About;
