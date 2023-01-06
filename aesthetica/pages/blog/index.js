62a3292f76ab499eb47eaa22fe6f1ff7cbf27391
vdumic <vdumic00@fesb.hr>
2023-01-06 13:06:45 +0100

    WIP: Blog page, button added

--- aesthetica/pages/blog/index.js
+++ aesthetica/pages/blog/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { SlArrowRight } from "react-icons/sl";
 import BigPost from "../../components/Blog/BigPost";
 import HeaderFooterLayout from "../../layouts/HeaderFooterLayout";
 
@@ -8,7 +10,17 @@ const Blog = () => {
         <p className="font-semibold text-5xl">BLOG</p>
       </div>
       <div className="flex justify-center my-10">
-        <BigPost />
+        <div>
+          <BigPost />
+          <div className="flex justify-center mt-10">
+            <Link href="/blog/older">
+              <button className="flex justify-center bg-bckgrnd-light hover:bg-bckgrnd text-black text-lg font-medium py-2 px-6 border-2 border-black rounded-full shadow-xl">
+                <p>Older posts</p>
+                <SlArrowRight className="h-6 w-6 sm:inline cursor-pointer pt-1" />
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </HeaderFooterLayout>
   );
