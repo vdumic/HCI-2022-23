d7a7e51541a71a121cb580e749ffc91a63b3ba14
vdumic <vdumic00@fesb.hr>
2023-01-14 17:11:57 +0100

    Responsive blog page

--- aesthetica/pages/blog/index.js
+++ aesthetica/pages/blog/index.js
@@ -4,6 +4,7 @@ import HeaderFooterLayout from "../../layouts/HeaderFooterLayout";
 import BigPost from "../../components/Blog/BigPost";
 import MediumPost from "../../components/Blog/MediumPost";
 import SmallPost from "../../components/Blog/SmallPost";
+import PostMobile from "../../components/Blog/PostMobile";
 
 import Image1 from "../../assets/blog1.png";
 import Image2 from "../../assets/blog2.png";
@@ -13,10 +14,10 @@ import Image4 from "../../assets/blog4.png";
 const Blog = () => {
   return (
     <HeaderFooterLayout title="Aesthetica / Blog">
-      <div className="flex justify-center my-10">
-        <p className="font-semibold text-5xl">BLOG</p>
+      <div className="flex justify-center my-10 sm:my-4">
+        <p className="font-semibold text-5xl sm:text-4xl">BLOG</p>
       </div>
-      <div className="flex justify-center my-10">
+      <div className="flex justify-center my-10 sm:hidden">
         <div>
           <BigPost
             title="Interior Design Trends for the Holiday Season."
@@ -41,13 +42,43 @@ const Blog = () => {
             image={Image4}
           />
           <div className="flex justify-center mt-20">
-            <Link href="/blog/older">
+            <Link href="/under_construction">
               <button className="flex justify-center bg-bckgrnd-light hover:bg-bckgrnd text-black text-lg font-medium py-2 px-6 border-2 border-black rounded-full shadow-xl">
                 <p>Older posts</p>
                 <SlArrowRight className="h-6 w-6 sm:inline cursor-pointer pt-1" />
               </button>
             </Link>
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center my-4 md:hidden">
+        <PostMobile
+          title="Interior Design Trends for the Holiday Season."
+          date="December 16, 2022"
+          image={Image1}
+        />
+        <PostMobile
+          title="Let Lighting Set the Tone of Your Space"
+          date="December 14, 2022"
+          image={Image2}
+        />
+        <PostMobile
+          title="Jewellery Storage Solutions"
+          date="December 9, 2022"
+          image={Image3}
+        />
+        <PostMobile
+          title="Smart Ways to Organize with Baskets for a Less Cluttered Home"
+          date="December 5, 2022"
+          image={Image4}
+        />
+        <div className="flex justify-center mt-8 mb-4">
+          <Link href="/under_construction">
+            <button className="flex justify-center bg-bckgrnd-light hover:bg-bckgrnd text-black text-m font-medium py-2 px-6 border-2 border-black rounded-full shadow-xl">
+              <p>Older posts</p>
+              <SlArrowRight className="h-5 w-5 cursor-pointer pt-1" />
+            </button>
+          </Link>
         </div>
       </div>
     </HeaderFooterLayout>
