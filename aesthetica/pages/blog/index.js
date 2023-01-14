ea28a3c190a86519b5cedc71a0f39c75e7af9d1e
David Čeko <cekodavid@gmail.com>
2023-01-14 18:36:24 +0100

    New desktop blog page layout added

--- aesthetica/pages/blog/index.js
+++ aesthetica/pages/blog/index.js
@@ -14,43 +14,62 @@ import Image4 from "../../assets/blog4.png";
 const Blog = () => {
   return (
     <HeaderFooterLayout title="Aesthetica / Blog">
-      <div className="flex justify-center my-10 sm:my-4">
+      <div className="flex justify-center mt-10 mb-6 sm:my-4">
         <p className="font-semibold text-5xl sm:text-4xl">BLOG</p>
       </div>
-      <div className="flex justify-center my-10 sm:hidden">
-        <div>
-          <BigPost
-            title="Interior Design Trends for the Holiday Season."
-            date="December 16, 2022"
-            image={Image1}
-          />
-          <div className="flex justify-center">
+
+      <div className="desktop-blog sm:hidden">
+        <div className="grid grid-cols-12 mx-24 gap-x-10">
+          <div className="grid col-span-7">
+            <BigPost
+              title="Interior Design Trends for the Holiday Season."
+              date="December 16, 2022"
+              image={Image1}
+            />
+          </div>
+          <div className="grid col-span-5">
             <MediumPost
-              title="Let Lighting Set the Tone of Your Space"
-              date="December 14, 2022"
+              title="Let Lighting Set the Tone of Your Space."
+              date="December 16, 2022"
               image={Image2}
             />
+          </div>
+        </div>
+        <div className="grid grid-cols-12 mx-24 gap-x-10">
+          <div className="grid col-span-4">
             <SmallPost
-              title="Jewellery Storage Solutions"
+              className="grid col-span-4"
+              title="Jewellery and Small Items Storage Solutions"
               date="December 9, 2022"
               image={Image3}
             />
           </div>
-          <BigPost
-            title="Smart Ways to Organize with Baskets for a Less Cluttered Home"
-            date="December 5, 2022"
-            image={Image4}
-          />
-          <div className="flex justify-center mt-20">
-            <Link href="/under_construction">
-              <button className="flex justify-center bg-bckgrnd-light hover:bg-bckgrnd text-black text-lg font-medium py-2 px-6 border-2 border-black rounded-full shadow-xl">
-                <p>Older posts</p>
-                <SlArrowRight className="h-6 w-6 sm:inline cursor-pointer pt-1" />
-              </button>
-            </Link>
+          <div className="grid col-span-4">
+            <SmallPost
+              className="grid col-span-4"
+              title="Smart Ways to Organize with Baskets"
+              date="December 5, 2022"
+              image={Image4}
+            />
+          </div>
+          <div className="grid col-span-4">
+            <SmallPost
+              title="Smart Ways to Organize with Baskets"
+              date="December 5, 2022"
+              image={Image4}
+            />
           </div>
         </div>
+        <div className="flex justify-center my-20">
+          <Link href="/under_construction">
+            <button className="flex justify-center bg-bckgrnd-light hover:bg-bckgrnd text-black text-lg font-medium py-2 px-6 border-2 border-black rounded-full shadow-xl">
+              <p>Older posts</p>
+              <SlArrowRight className="h-6 w-6 sm:inline cursor-pointer pt-1" />
+            </button>
+          </Link>
+        </div>
       </div>
+
       <div className="flex flex-col justify-center my-4 md:hidden">
         <PostMobile
           title="Interior Design Trends for the Holiday Season."
