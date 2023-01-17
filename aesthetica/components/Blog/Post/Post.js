7e85002eb8b9ce814429ffd470be16d4db1a5fa2
vdumic <vdumic00@fesb.hr>
2023-01-17 12:44:18 +0100

    WIP: Blog post page all styles removed

--- aesthetica/components/Blog/Post/Post.js
+++ aesthetica/components/Blog/Post/Post.js
@@ -1,13 +1,24 @@
 import Image from "next/image";
+import { MDXRemote } from "next-mdx-remote";
+import { H3, P, Ul, Ol, Li } from "../Post/components";
 
-const Post = ({ title, date, image }) => {
+const components = {
+  h3: H3,
+  p: P,
+  ul: Ul,
+  ol: Ol,
+  li: Li,
+};
+
+const Post = ({ post }) => {
   return (
     <div>
       <div className="flex justify-between">
-        <p className="text-5xl font-semibold mb-10">{title}</p>
-        <p className="text-blog-text text-xl font-semibold my-5">{date}</p>
+        <p className="text-5xl font-semibold mb-10">{post.title}</p>
+        <p className="text-blog-text text-xl font-semibold my-5">{post.date}</p>
       </div>
-      <Image alt="post" src={image} height="550" width="500" />
+      <Image alt="post" src={post.image.url} height="550" width="500" />
+      <MDXRemote {...post.mdxSource} components={components} lazy />
     </div>
   );
 };
--- aesthetica/pages/blog/[slug].js
+++ aesthetica/pages/blog/[slug].js
@@ -1,18 +1,8 @@
 import { serialize } from "next-mdx-remote/serialize";
-import { MDXRemote } from "next-mdx-remote";
 import HeaderFooterLayout from "../../layouts/HeaderFooterLayout";
 import Post from "../../components/Blog/Post/Post";
-import { H3, Ul, Ol, Li, P } from "../../components/Blog/Post/components";
 
 import { getAllPostSlugs, getPostBySlug } from "../api/ContentfulAPI";
-
-const components = {
-  h3: H3,
-  p: P,
-  ul: Ul,
-  ol: Ol,
-  li: Li,
-};
 
 const BlogPost = ({ post }) => {
   return (
@@ -23,9 +13,9 @@ const BlogPost = ({ post }) => {
           date={post.date}
           image={post.image.url}
           content={post.content}
+          post={post}
         />
       </div>
-      <MDXRemote {...post.mdxSource} components={components} lazy />
     </HeaderFooterLayout>
   );
 };
