24c52021fdcca1b3600bdfdee442bab2472178f3
vdumic <vdumic00@fesb.hr>
2023-01-18 21:12:30 +0100

    WIP: Category page added

--- aesthetica/pages/store/category/[slug].js
+++ aesthetica/pages/store/category/[slug].js
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { AiOutlineRight } from "react-icons/ai";
+
 import HeaderFooterLayout from "../../../layouts/HeaderFooterLayout";
 import {
   getAllCategorySlugs,
@@ -7,7 +10,19 @@ import {
 const CategoryPage = ({ products }) => {
   return (
     <HeaderFooterLayout title="Aesthetica / Store">
-      <div>CATEGORY</div>
+      <div className="flex justify-start max-w-full mx-24 sm:mx-8 lg:my-10 sm:my-6">
+        <Link href={`/store/${products[0].roomSlug}`}>
+          <p className="font-bold sm:text-2xl text-3xl text-[#777777]">
+            {products[0].room}
+          </p>
+        </Link>
+        <AiOutlineRight className="h-7 w-7 sm:inline sm:w-5 sm:h-5 lg:mt-1 sm:mt-2" />
+        <Link href={`/store/category/${products[0].categorySlug}`}>
+          <p className="font-bold sm:text-2xl text-3xl">
+            {products[0].category}
+          </p>
+        </Link>
+      </div>
     </HeaderFooterLayout>
   );
 };
