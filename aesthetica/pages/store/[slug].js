2d6adeb937a5a8cfb6b5045f9f250f35a25ea3a4
vdumic <vdumic00@fesb.hr>
2023-01-22 18:36:20 +0100

    Product search functional

--- aesthetica/pages/store/[slug].js
+++ aesthetica/pages/store/[slug].js
@@ -1,29 +1,64 @@
-import { SlArrowRight } from "react-icons/sl";
+import { useState } from "react";
 import Link from "next/link";
+import { SlArrowRight } from "react-icons/sl";
+import { AiOutlineSearch } from "react-icons/ai";
 
-import CategoriesSlider from "../../components/Store/Category/Room/CategoriesSlider";
-import HeaderFooterLayout from "../../layouts/HeaderFooterLayout";
-import StoreSearch from "../../components/Store/StoreSearch";
-import ItemsList from "../../components/Store/Category/ItemsList";
 import {
   getAllCategoriesByRoom,
   getAllRoomSlugs,
   getAllProductsByRoom,
 } from "../api/ContentfulAPI";
+
+import CategoriesSlider from "../../components/Store/Category/Room/CategoriesSlider";
+import HeaderFooterLayout from "../../layouts/HeaderFooterLayout";
+import ItemsList from "../../components/Store/Category/ItemsList";
 import Filter from "../../components/Filters/Filter";
 
 const RoomPage = ({ products, categories }) => {
+  const [showProducts, setShowProducts] = useState(products);
+  const [filterValue, setFilterValue] = useState("");
+
+  const handleInput = (e) => {
+    setFilterValue(e.target.value);
+
+    setShowProducts(
+      e.target.value == ""
+        ? products
+        : products.filter((product) => {
+            return product.title
+              .toLowerCase()
+              .includes(e.target.value.toLowerCase());
+          })
+    );
+  };
+
   return (
     <HeaderFooterLayout title="Aesthetica / Store">
       <div className="flex justify-start max-w-full mx-24 sm:mx-8 lg:my-10 sm:my-6">
         <p className="font-bold sm:text-2xl text-3xl">{categories[0].room}</p>
       </div>
       <CategoriesSlider categories={categories} />
-      <StoreSearch />
+      <div className="flex items-center max-w-xl lg:my-4 mx-auto bg-white sm:my-5 my-16 sm:mx-8">
+        <input
+          type="search"
+          id="support-search"
+          className="block w-full p-4 pl-10 text-m placeholder-gray-900 border-white"
+          placeholder="Search products..."
+          value={filterValue}
+          onChange={handleInput}
+          required
+        />
+        <button
+          type="submit"
+          className="bg-white right-2.5 bottom-2.5 text-black  px-4 py-2"
+        >
+          <AiOutlineSearch className="h-7 w-7 sm:inline cursor-pointer" />
+        </button>
+      </div>
       <div className="flex flex-row">
         <Filter />
         <div className="flex justify-evenly sm:mx-8 mx-24 my-10">
-          <ItemsList products={products} />
+          <ItemsList products={showProducts} />
         </div>
       </div>
       <div className="flex justify-center my-10">
--- aesthetica/pages/store/category/[slug].js
+++ aesthetica/pages/store/category/[slug].js
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import Link from "next/link";
-import { AiOutlineRight } from "react-icons/ai";
+import { AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
 
 import {
   getAllCategorySlugs,
@@ -7,11 +8,27 @@ import {
 } from "../../api/ContentfulAPI";
 
 import HeaderFooterLayout from "../../../layouts/HeaderFooterLayout";
-import StoreSearch from "../../../components/Store/StoreSearch";
 import ItemsList from "../../../components/Store/Category/ItemsList";
 import Filter from "../../../components/Filters/Filter";
 
 const CategoryPage = ({ products }) => {
+  const [showProducts, setShowProducts] = useState(products);
+  const [filterValue, setFilterValue] = useState("");
+
+  const handleInput = (e) => {
+    setFilterValue(e.target.value);
+
+    setShowProducts(
+      e.target.value == ""
+        ? products
+        : products.filter((product) => {
+            return product.title
+              .toLowerCase()
+              .includes(e.target.value.toLowerCase());
+          })
+    );
+  };
+
   return (
     <HeaderFooterLayout title="Aesthetica / Store">
       <div className="flex justify-start max-w-full mx-24 sm:mx-8 lg:my-10 sm:my-6">
@@ -23,11 +40,27 @@ const CategoryPage = ({ products }) => {
         <AiOutlineRight className="h-7 w-7 sm:inline sm:w-5 sm:h-5 lg:mt-1 sm:mt-2" />
         <p className="font-bold sm:text-2xl text-3xl">{products[0].category}</p>
       </div>
-      <StoreSearch />
+      <div className="flex items-center max-w-xl lg:my-4 mx-auto bg-white sm:my-5 my-16 sm:mx-8">
+        <input
+          type="search"
+          id="support-search"
+          className="block w-full p-4 pl-10 text-m placeholder-gray-900 border-white"
+          placeholder="Search products..."
+          value={filterValue}
+          onChange={handleInput}
+          required
+        />
+        <button
+          type="submit"
+          className="bg-white right-2.5 bottom-2.5 text-black  px-4 py-2"
+        >
+          <AiOutlineSearch className="h-7 w-7 sm:inline cursor-pointer" />
+        </button>
+      </div>
       <div className="flex flex-row">
         <Filter />
         <div className="flex justify-evenly sm:mx-8 mx-24 my-10">
-          <ItemsList products={products} />
+          <ItemsList products={showProducts} />
         </div>
       </div>
     </HeaderFooterLayout>
